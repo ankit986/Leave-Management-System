ab35d0f10c411041c0fa1ad7be2742b76378ce8f
ankit986 <ab98647@gmail.com>
2019-12-31 17:33:05 +0530

    First Upload

---
+++ lms_2/admin/search_staff_for_updation.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Under Construction</title>
+</head>
+<body>
+	<h1>Page under construction!</h1>
+<style type="text/css">
+	body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-image: url(1.jpg);
+}
+h1{
+    color: black;
+    text-align: center;
+    border: 5px solid black;
+    
+    width: 300px;
+    padding: 20px;
+    margin: 0 auto;
+    margin-top: 200px;
+    font-family: 'Oswald', sans-serif;
+    font-size: 3em;
+}
+
+</style>
+</body>
+</html>
